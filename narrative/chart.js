8ae998272f422311757a7770ba7135dea42dd9e6
acdreyer <ec2-user@ip-172-31-38-74.us-east-2.compute.internal>
2019-10-14 04:03:32 +0000

    Labels

--- narrative/chart.js
+++ narrative/chart.js
@@ -1,16 +1,23 @@
 // Request the data
-d3.json('./data/data.json', function(err, response){
-
+// d3.json('./data/data.json', function(err, response) {
+d3.json('./prepareDataset/narrVizData.json', function(err, response) {
+	
 	var svg, scenes, charactersMap, width, height, sceneWidth;
+	var fpath = './prepareDataset/downloads/'
 
 	// Get the data in the format we need to feed to d3.layout.narrative().scenes
 	scenes = wrangle(response);
 
+// console.log(scenes);
 	// Some defaults
-	sceneWidth = 30;
-	width = scenes.length * sceneWidth * 4;
+	sceneWidth = 45;
+	width = scenes.length * sceneWidth * 3;
 	height = 700;
-	labelSize = [150,15];
+	labelSize = [150, 15];
+
+
+
+
 
 	// The container element (this is the HTML fragment);
 	svg = d3.select("body").append('svg')
@@ -18,14 +25,27 @@ d3.json('./data/data.json', function(err, response){
 		.attr('width', width)
 		.attr('height', height);
 
+	// -----------------------------------------------------------
+	// // Draw the outerbox
+	svg
+	.selectAll('rect')
+		.data([null])
+		.enter()
+		.append('rect')
+		.attr('class', 'boundingbox')
+		.attr('width', width)
+		.attr('height', height+10)
+		.style('background','#c1c1c1');
+
+
 	// Calculate the actual width of every character label.
-	scenes.forEach(function(scene){
+	scenes.forEach(function(scene) {
 		scene.characters.forEach(function(character) {
 			character.width = svg.append('text')
-				.attr('opacity',0)
+				.attr('opacity', 0)
 				.attr('class', 'temp')
 				.text(character.name)
-					.node().getComputedTextLength()+10;
+				.node().getComputedTextLength() + 10;
 		});
 	});
 
@@ -35,126 +55,35 @@ d3.json('./data/data.json', function(err, response){
 	// Do the layout
 	narrative = d3.layout.narrative()
 		.scenes(scenes)
-		.size([width,height])
-		.pathSpace(25)
+		.size([width, height])
+		.pathSpace(30)				//space between images
 		.groupMargin(10)
-		.labelSize([250,15])
-		.scenePadding([5,sceneWidth/2,5,sceneWidth/2])
+		.labelSize([100, 18])
+		.scenePadding([20, sceneWidth / 2, 20, sceneWidth / 2])
 		.labelPosition('left')
 		.layout();
+
+
+// construct new dataset for the viz with images
+var obj = narrative.scenes();
+console.log(obj.length);
+for (let i =0; i < obj.length; i++){
+	obj[i].label = response.halfcentrs[i];
+	obj[i].images = response.scnimages[i];
+}
+console.log(obj);
+
+
 
 	// Get the extent so we can re-size the SVG appropriately.
 	svg.attr('height', narrative.extent()[1]);
 
-	// Draw the scenes
-	svg.selectAll('.scene').data(narrative.scenes()).enter()
-		.append('g').attr('class', 'scene')
-			.attr('transform', function(d){
-					var x,y;
-					x = Math.round(d.x)+0.5;
-					y = Math.round(d.y)+0.5;
-					return 'translate('+[x,y]+')';
-				})
-			.append('rect')
-				.attr('width', sceneWidth*0.1)
-				.attr('height', function(d){
-					return d.height;
-				})
-				.attr('y', 0)
-				.attr('x', sceneWidth*0.45)
-				.attr('rx', 0)
-				.attr('ry', 0)
-			.append('xlabel')
-				.attr('class','text')
-				.attr('y', '10px')
-				.attr('x', '-20px')
-				.text('Label test');
 
 
-	// Draw appearances
-	svg.selectAll('.scene').data(narrative.scenes()).enter()
-	.append('circle')
-		.attr('cx', function(d){
-			return d.x;
-		})
-		.attr('cy', function(d){
-			return d.y;
-		})
-		.attr('r', function(){
-			return 1;
-		})
-		.attr('class', function(d){
-			return 'appearance ' + d.character.affiliation;
-		});
-		
-		
-		
-		
-		
-	// Draw labels
-	svg.selectAll('.scene').selectAll('.appearance').data(function(d){
-		return d.appearances;
-	}).enter().call(function(s){
-			var g, text;
 
-			g = s.append('g').attr('class', 'label');
 
-			// g.append('rect')
-			// 	.attr('y', -4)
-			// 	.attr('x', -4)
-			// 	.attr('width', 4)
-			// 	.attr('height', 8);
 
-			g.append('svg:image')
-				.attr('y', -30)
-				.attr('x', -10)
-				.attr('width', 225*0.15)
-				.attr('height', 400*0.15)
-				.attr('xlink:href', './images/Image1.jpg');
-				
-				
-			text = g.append('g').attr('class','text');
-
-			// Apppend two actual 'text' nodes to fake an 'outside' outline.
-			text.append('text');
-			text.append('text').attr('class', 'color');
-
-			g.attr('transform', function(d){
-					var x,y;
-					x = Math.round(d.x);
-					y = Math.round(d.y);
-					return 'translate(' + [x,y] + ')';
-				});
-
-			// g.selectAll('text')
-			// 	.attr('text-anchor', 'end')
-			// 	.attr('y', '5px')
-			// 	.attr('x', '-20px')
-			// 	.text(function(d){ return d.character.name; });
-
-			// g.select('.color')
-			// 	.attr('class', function(d){
-			// 		return 'color ' + d.character.affiliation;
-			// 	});
-
-			// g.select('rect')
-			// 	.attr('class', function(d){
-			// 		return d.character.affiliation;
-			// 	});
-
-		});
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
+	// ------------------links
 	// Draw links
 	svg.selectAll('.link').data(narrative.links()).enter()
 		.append('path')
@@ -165,6 +94,87 @@ d3.json('./data/data.json', function(err, response){
 
 
 
+	// Draw the lines to divide times
+	svg.selectAll('.periodlines').data(narrative.scenes()).enter()
+		.append('g').attr('class', 'periodlines')
+		.attr('transform', function(d) {
+			var x, y;
+			x = Math.round(d.x) ;//+ 0.5;
+			// y = Math.round(d.y) + 0.5;
+			y = 0;
+			return 'translate(' + [x, y] + ')';
+		})
+		.append('line')
+		.attr('x1', 0)
+		.attr('y1', 0)
+		.attr('x2', 0)
+		.attr('y2', height);
+
+
+
+
+
+	// -----------------------------------------------------------
+	// Draw the scenes
+	svg.selectAll('.scene').data(narrative.scenes()).enter()
+		.append('g').attr('class', 'scene')
+		.attr('transform', function(d) {
+			var x, y;
+			x = Math.round(d.x) + 0.5;
+			y = Math.round(d.y) + 0.5;
+			return 'translate(' + [x, y] + ')';
+		})
+		// .append('rect')
+		// .attr('width', sceneWidth*1.2)
+		// .attr('height', function(d) {
+		// 	return d.height*0.8;
+		// })
+		.attr('y', 10)
+		.attr('x', -sceneWidth*0.5*0 )
+		.attr('rx', 0)
+		.attr('ry', 0)
+	// .append('xlabel')
+	// 	.attr('class','text')
+	// 	.attr('y', '10px')
+	// 	.attr('x', '-20px')
+	// 	.text('Label test');
+
+	// -----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+// console.log(narrative.scenes());
+
+	// Draw the time legends
+	// svg.selectAll('.periodtext').data(narrative.scenes()).enter()
+	svg.selectAll('.periodtext').data(obj).enter()
+		.append('text').attr('class', 'text')
+		.attr('transform', function(d) {
+			var x, y;
+			x = Math.round(d.x)+25;
+			y = 0*Math.round(d.y);
+			return 'translate(' + [x, y] + ')';
+		})
+		// .append('rect')
+		// .attr('width', sceneWidth * 2)
+		// .attr('height', '12px')
+		// .attr('y', 0)
+		// .attr('x', -sceneWidth * 0.5)
+		// .attr('class', 'text')
+		.attr('y', '10px')
+		.attr('x', '-20px')
+		.text( function(d){ return d.label   }  );
+
+
+	// -----------------------------------------------------------
 
 
 
@@ -189,12 +199,86 @@ d3.json('./data/data.json', function(err, response){
 
 
 
+	// -----------------------------------------------------------
+	// Draw appearances
+	svg.selectAll('.scene').selectAll('.appearance').data(function(d){
+		return d.appearances;
+	}).enter().append('circle')
+		.attr('cx', function(d){
+			return d.x;
+		})
+		.attr('cy', function(d){
+			return d.y;
+		})
+		.attr('r', function(){
+			return 2;
+		})
+		.attr('class', function(d){
+			return 'appearance ' + d.character.affiliation;
+		});
 
 
+
+
+
+
+
+
+
+
+
+
+
+	// Draw figures at time periods
+	svg.selectAll('.scene').selectAll('.appearance2').data(function(d) {
+		return d.appearances;
+	}).enter().call(function(s) {
+		var g, text;
+
+		g = s.append('g').attr('class', 'label');
+
+		g.append('svg:image')
+			.attr('y', -30)
+			.attr('x', -sceneWidth*0.5 )
+			.attr('width', sceneWidth)
+			.attr('height', 400 * 0.15)
+			.attr('transform', 'rotate(90 0 0)')
+			// .attr('xlink:href', './images/Image1.jpg');
+			// .attr('xlink:href', './images/14.25.32_002dec2014.jpg');
+			.attr('xlink:href', 'fff');
+			// .text( function(d){ return d.images  } );
+
+
+
+		text = g.append('g').attr('class', 'text');
+
+		// Apppend two actual 'text' nodes to fake an 'outside' outline.
+		text.append('text');
+		text.append('text').attr('class', 'color')
+		// .text(function(d){ return 'fpath' + d.images.imagenames[0]  })
+
+		g.attr('transform', function(d) {
+			var x, y;
+			x = Math.round(d.x);
+			y = Math.round(d.y);
+			return 'translate(' + [x, y] + ')';
+		});
+
+
+
+	});
+
+
+
+
+
+
+
+// ----------------------------------------------
 
 	// Draw intro nodes
 	svg.selectAll('.intro').data(narrative.introductions())
-		.enter().call(function(s){
+		.enter().call(function(s) {
 			var g, text;
 
 			g = s.append('g').attr('class', 'intro');
@@ -205,40 +289,40 @@ d3.json('./data/data.json', function(err, response){
 			// 	.attr('width', 4)
 			// 	.attr('height', 8);
 
-			g.append('svg:image')
-				.attr('y', -30)
-				.attr('x', -10)
-				.attr('width', 225*0.15)
-				.attr('height', 400*0.15)
-				.attr('xlink:href', './images/Image1.jpg');
-				
-				
-			text = g.append('g').attr('class','text');
+			// g.append('svg:image')
+			// 	.attr('y', -30)
+			// 	.attr('x', -10)
+			// 	.attr('width', 225 * 0.15)
+			// 	.attr('height', 400 * 0.15)
+			// 	.attr('xlink:href', './images/Image1.jpg');
+
+
+			text = g.append('g').attr('class', 'text');
 
 			// Apppend two actual 'text' nodes to fake an 'outside' outline.
 			text.append('text');
 			text.append('text').attr('class', 'color');
 
-			g.attr('transform', function(d){
-					var x,y;
-					x = Math.round(d.x);
-					y = Math.round(d.y);
-					return 'translate(' + [x,y] + ')';
-				});
+			g.attr('transform', function(d) {
+				var x, y;
+				x = Math.round(d.x);
+				y = Math.round(d.y);
+				return 'translate(' + [x, y] + ')';
+			});
 
 			g.selectAll('text')
 				.attr('text-anchor', 'end')
 				.attr('y', '5px')
 				.attr('x', '-20px')
-				.text(function(d){ return d.character.name; });
+				.text(function(d) { return d.character.name; });
 
 			g.select('.color')
-				.attr('class', function(d){
+				.attr('class', function(d) {
 					return 'color ' + d.character.affiliation;
 				});
 
 			g.select('rect')
-				.attr('class', function(d){
+				.attr('class', function(d) {
 					return d.character.affiliation;
 				});
 
@@ -250,16 +334,18 @@ function wrangle(data) {
 
 	var charactersMap = {};
 
-	return data.scenes.map(function(scene){
-		return {characters: scene.map(function(id){
-			return characterById(id);
-		}).filter(function(d) { return (d); })};
+	return data.scenes.map(function(scene) {
+		return {
+			characters: scene.map(function(id) {
+				return characterById(id);
+			}).filter(function(d) { return (d); })
+		};
 	});
 
 	// Helper to get characters by ID from the raw data
 	function characterById(id) {
 		charactersMap = charactersMap || {};
-		charactersMap[id] = charactersMap[id] || data.characters.find(function(character){
+		charactersMap[id] = charactersMap[id] || data.characters.find(function(character) {
 			return character.id === id;
 		});
 		return charactersMap[id];
--- narrative/prepareDataset/node_prepdata.js
+++ narrative/prepareDataset/node_prepdata.js
@@ -55,6 +55,8 @@ Json format:
 const fs = require('fs');
 var async = require('async');
 var d3 = require("d3");
+
+
 // Specify filenames
 var datalist = './data/metArmsFromAPI.json';
 var fnamewrite = './narrVizData.json'
@@ -76,30 +78,32 @@ var n;
 // Specify categories that need to be used in final plot:
 
 // The timeline grouped by half centuries:
-var halfcentrs = [1300, 1350, 1400, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850];
+var halfcentrs = [1300, 1350, 1400, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800];
 
-// Declare scenehold as array of arrays
+// Declare array of arrays
 var scenehold = new Array(halfcentrs.length);
-halfcentrs.forEach(function(value,index){
-    scenehold[index] = [];
-})
+halfcentrs.forEach(function(value, index) { scenehold[index] = []; });
+var scnimghold = new Array(halfcentrs.length);
+halfcentrs.forEach(function(value, index) { scnimghold[index] = []; });
+var scnMETurlshold = new Array(halfcentrs.length);
+halfcentrs.forEach(function(value, index) { scnMETurlshold[index] = []; });
 // console.log(halfcentrs.length);
-let finaldata = { characters: [], scenes: [], scnimages: [], halfcentrs };
+let finaldata = { characters: [], scenes: [], scnimages: [], scnMETurls: [], halfcentrs };
 
 // The types of weapons and armor (i.e. the "characters" in this narrative timeline)
 let characterlist = ["Halberd",
     "Guisarme",
     "Fauchard",
     "Partisan",
-    "Military fork",
+    // "Military fork",
     "Barbute",
     "Bascinet",
-    "Visored bascinet",
+    // "Visored bascinet",
     "Armet",
     "Sallet",
     "Visored sallet",
     "Burgonet",
-    "Closed burgonet",
+    // "Closed burgonet",
     "Lobster-tail burgonet",
     "Morion",
     "Morion-cabasset",
@@ -110,9 +114,9 @@ let characterlist = ["Halberd",
     "Rapier",
     "Cup-hilted rapier",
     "Basket-hilted sword",
-    "Hunting sword",
+    // "Hunting sword",
     "Claymore",
-    "Smallsword",
+    // "Smallsword",
     // "Two-Hand Sword",
     // "Two-Handed Sword",
     "Breech-loading needlefire rifle",
@@ -125,13 +129,19 @@ let characterlist = ["Halberd",
 // console.log(characterlist.length)
 
 // The grouping of each type of "character"; weapon or helmet
-let staffwpn = ["Halberd", "Guisarme", "Fauchard", "Partisan", "Military fork"];
-let helmet = ["Barbute", "Bascinet", "Visored bascinet", "Armet", "Sallet", "Visored sallet",
-    "Burgonet", "Closed burgonet", "Lobster-tail burgonet", "Morion", "Morion-cabasset", "Cabasset",
+let staffwpn = ["Halberd", "Guisarme", "Fauchard", "Partisan"//, "Military fork"
+];
+let helmet = ["Barbute", "Bascinet",
+    //"Visored bascinet", 
+    "Armet", "Sallet", "Visored sallet",
+    "Burgonet",
+    //"Closed burgonet", 
+    "Lobster-tail burgonet", "Morion", "Morion-cabasset", "Cabasset",
     // "War Hat",  //#remove japanese remove Cambodian// # "Spider Helmet",
     "Siege helmet"
 ];
-let other = ["Rapier", "Cup-hilted rapier", "Basket-hilted sword", "Hunting sword", "Claymore", "Smallsword",
+let other = ["Rapier", "Cup-hilted rapier", "Basket-hilted sword", //"Hunting sword", 
+"Claymore"//, //"Smallsword",
     // "Two-Hand Sword",
     // "Two-Handed Sword",
 ];
@@ -196,55 +206,96 @@ catch (err) { console.log(err) }
 finally {
 
 
-
-    // halfcentrs.forEach(function(value,index){
-    //     var d3.obj
-    // })
-
-
     // Determine the extent of date ranges for each character
     characterlist.forEach(function(value, index) {
-        var filtdata = obj.filter(function(d) { return d.objectName == value });
+
+
+        // create a dataset filtered that only includes characters
+        var filtdata1 = obj.filter(function(d) { return d.objectEndDate <= 1900 });
+        var filtdata = filtdata1.filter(function(d) { return d.objectName == value });
+
+
+        // check date/year ranges and store min/max for this character
         var mindate = d3.min(filtdata, function(d) { return +d.objectBeginDate; });
         var maxdate = d3.max(filtdata, function(d) { return +d.objectBeginDate; });
         // console.log(value + ' from ' + mindate + ' to ' + maxdate)
         charactersStartYear[index] = mindate;
         charactersEndYear[index] = maxdate;
-        
-        // Create the scene lists from contribution of characters by looping through dates
+        var avgdate = 0.5 * (mindate + maxdate);
+        // console.log(filtdata[0].fnImageSmall);
+
+
+        // Create the scene lists from contribution of characters by looping through years/halfcenturies
+        // also create two additional arrays that have the char names with image filenames and MET urls
         halfcentrs.forEach(function(val2, indx2) {
-        if (mindate <= val2 && maxdate >= val2){
-            // temparray.push(value)
-            scenehold[indx2].push(value);
-        }
+
+            let tempSceneCharacter = { character: [], imagenames: [], time: [] }; //init temporary storage
+            let tempSceneCharURLs = { character: [], itemsURL: [], time: [] }; //init temporary storage
+
+            if (mindate <= val2 && maxdate >= val2) { // check that character is in this scene
+
+                scenehold[indx2].push(value); //add the character name to the scene
+                tempSceneCharacter.character = value; // add the character name to imagefile object
+                tempSceneCharURLs.character = value;
+
+                tempSceneCharacter.time = val2; // add the character name to imagefile object
+                tempSceneCharURLs.time = val2;
+                // Go through each MET item and add image if it is within year range
+                // also build in check to add image of previous range for halfcentury image range gaps
+                filtdata.forEach(function(val3, indx3) {
+                    if (val3.objectBeginDate <= val2 && val3.objectEndDate >= val2) { // is this item of this character in this scene?
+                        tempSceneCharacter.imagenames.push(val3.fnImageSmall);
+                        tempSceneCharURLs.itemsURL.push(val3.objectURL);
+                    }
+                });
+                scnimghold[indx2].push(tempSceneCharacter);
+                scnMETurlshold[indx2].push(tempSceneCharURLs);
+            };
 
         });
+        // console.log(charactersStartYear);
+
+
+        // Assign data to finaldata
+        // console.log(scnimghold[1])
+        finaldata.scenes = scenehold;
+        finaldata.scnimages = scnimghold;
+        finaldata.scnMETurls = scnMETurlshold;
 
 
     });
-    // console.log(charactersStartYear);
-    
-    // Assign data to finaldata
-    finaldata.scenes = scenehold;
 
 
+    // go through the scenes/years again and clean image names (fill gaps and choose which ones to use in final viz)
+    for (let i=0; i < (finaldata.scnimages.length-2); i++) {
+        
+        finaldata.scnimages[i].forEach(function(val5, indx5) {
 
+                if (val5.imagenames == '') {
+                var previousindex = finaldata.scnimages[i-1].find(function(element){
+                return element.character == val5.character;
+                });
+                // var nextindex = finaldata.scnimages[i+1].find(function(element){
+                //     return element.character == val5.character;
+                // });
+                
+                // if (val5.imagenames == '' && previousindex.scnimages) 
+                // {finaldata.scnimages[i][indx5].imagenames = previousindex.scnimages.imagenames} 
+                // else if  (nextindex.scnimages){val5.imagenames = previousindex.scnimages} 
+                
+                console.log("*********");
+                // console.log(previousindex)
+                console.log(val5);
+                // console.log(finaldata.scnimages[i][indx5])
+                // console.log(nextindex)
+                console.log("*********")
+                // console.log(val5);
+            };
+        });
+    };
 
+    // console.log(finaldata.scnimages[5]);
 
+    fs.writeFileSync(fnamewrite, JSON.stringify(finaldata), null, "\n");
 
-
-
-
-};
-
-
-
-console.log(finaldata);
-
-fs.writeFileSync(fnamewrite, JSON.stringify(finaldata), null,"\n");
-
-
-
-
-
-
+}
