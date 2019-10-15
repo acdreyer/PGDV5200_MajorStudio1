9b6b46bd210d583eef31fab78dfcfd3cbcca00ea
acdreyer <ec2-user@ip-172-31-38-74.us-east-2.compute.internal>
2019-10-15 01:32:56 +0000

    Layout working

--- narrative/chart.js
+++ narrative/chart.js
@@ -1,19 +1,21 @@
 // Request the data
 // d3.json('./data/data.json', function(err, response) {
 d3.json('./prepareDataset/narrVizData.json', function(err, response) {
-	
+
 	var svg, scenes, charactersMap, width, height, sceneWidth;
-	var fpath = './prepareDataset/downloads/'
+	// var fpath = './prepareDataset/downloads/'
+	var fpath = './images/'
 
 	// Get the data in the format we need to feed to d3.layout.narrative().scenes
 	scenes = wrangle(response);
 
-// console.log(scenes);
+	console.dir(scenes);
+
 	// Some defaults
 	sceneWidth = 45;
 	width = scenes.length * sceneWidth * 3;
-	height = 700;
-	labelSize = [150, 15];
+	height =700;
+	labelSize = [100, 20];
 
 
 
@@ -28,14 +30,14 @@ d3.json('./prepareDataset/narrVizData.json', function(err, response) {
 	// -----------------------------------------------------------
 	// // Draw the outerbox
 	svg
-	.selectAll('rect')
+		.selectAll('rect')
 		.data([null])
 		.enter()
 		.append('rect')
 		.attr('class', 'boundingbox')
 		.attr('width', width)
-		.attr('height', height+10)
-		.style('background','#c1c1c1');
+		.attr('height', height + 20)
+		.style('background', '#c1c1c1');
 
 
 	// Calculate the actual width of every character label.
@@ -56,7 +58,7 @@ d3.json('./prepareDataset/narrVizData.json', function(err, response) {
 	narrative = d3.layout.narrative()
 		.scenes(scenes)
 		.size([width, height])
-		.pathSpace(30)				//space between images
+		.pathSpace(30) //space between images
 		.groupMargin(10)
 		.labelSize([100, 18])
 		.scenePadding([20, sceneWidth / 2, 20, sceneWidth / 2])
@@ -64,23 +66,64 @@ d3.json('./prepareDataset/narrVizData.json', function(err, response) {
 		.layout();
 
 
-// construct new dataset for the viz with images
-var obj = narrative.scenes();
-console.log(obj.length);
-for (let i =0; i < obj.length; i++){
-	obj[i].label = response.halfcentrs[i];
-	obj[i].images = response.scnimages[i];
-}
-console.log(obj);
+	// ------------------------------------------------------------------------
 
+	// construct new dataset for the viz with images
+	try {
+		var obj = narrative.scenes();
+		console.log(obj.length);
+		console.log(obj);
+		for (let i = 0; i < obj.length; i++) {
+			obj[i].label = response.halfcentrs[i];
+			obj[i].images = response.scnimages[i];
+		}
+	}
+	catch (err) { console.log(err) };
+
+
+// Match the object names in each scene with the image filenames
+	for (let i = 0; i < obj.length; i++) {
+
+		obj[i].appearances.forEach(function(val, ind) {
+			obj[i].appearances[ind].time = obj[i].images[ind].time;		//assign time period
+
+			let j = obj[i].images.findIndex(function(element) {		//check the item
+				return element.character == val.character.name;
+			});
+			
+			obj[i].appearances[ind].imagename = obj[i].images[j].imagenames[0]; //add the filename
+			
+			
+			if (obj[i].images[j].imagenames[0] == null){	//if filename empty, add from previous time range
+			obj[i].appearances[ind].imagename = obj[i-1].appearances[ind].imagename
+			} 
+			
+		})
+	};
+
+	// console.log(obj[0].appearances)
+	// console.log(obj[0].images)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// -----------------------------------------------------------------------
+	// do the drawing
 
 
 	// Get the extent so we can re-size the SVG appropriately.
 	svg.attr('height', narrative.extent()[1]);
-
-
-
-
 
 
 	// ------------------links
@@ -99,7 +142,7 @@ console.log(obj);
 		.append('g').attr('class', 'periodlines')
 		.attr('transform', function(d) {
 			var x, y;
-			x = Math.round(d.x) ;//+ 0.5;
+			x = Math.round(d.x); //+ 0.5;
 			// y = Math.round(d.y) + 0.5;
 			y = 0;
 			return 'translate(' + [x, y] + ')';
@@ -130,37 +173,20 @@ console.log(obj);
 		// 	return d.height*0.8;
 		// })
 		.attr('y', 10)
-		.attr('x', -sceneWidth*0.5*0 )
+		.attr('x', -sceneWidth * 0.5 * 0)
 		.attr('rx', 0)
 		.attr('ry', 0)
-	// .append('xlabel')
-	// 	.attr('class','text')
-	// 	.attr('y', '10px')
-	// 	.attr('x', '-20px')
-	// 	.text('Label test');
 
 	// -----------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-// console.log(narrative.scenes());
 
 	// Draw the time legends
 	// svg.selectAll('.periodtext').data(narrative.scenes()).enter()
 	svg.selectAll('.periodtext').data(obj).enter()
-		.append('text').attr('class', 'text')
+		.append('text').attr('class', 'periodtext')
 		.attr('transform', function(d) {
 			var x, y;
-			x = Math.round(d.x)+25;
-			y = 0*Math.round(d.y);
+			x = Math.round(d.x) + 25;
+			y = 0 * Math.round(d.y);
 			return 'translate(' + [x, y] + ')';
 		})
 		// .append('rect')
@@ -171,8 +197,7 @@ console.log(obj);
 		// .attr('class', 'text')
 		.attr('y', '10px')
 		.attr('x', '-20px')
-		.text( function(d){ return d.label   }  );
-
+		.text(function(d) { return d.label });
 
 	// -----------------------------------------------------------
 
@@ -201,19 +226,19 @@ console.log(obj);
 
 	// -----------------------------------------------------------
 	// Draw appearances
-	svg.selectAll('.scene').selectAll('.appearance').data(function(d){
-		return d.appearances;
-	}).enter().append('circle')
-		.attr('cx', function(d){
+	svg.selectAll('.scene').selectAll('.appearance').data(function(d) {
+			return d.appearances;
+		}).enter().append('circle')
+		.attr('cx', function(d) {
 			return d.x;
 		})
-		.attr('cy', function(d){
+		.attr('cy', function(d) {
 			return d.y;
 		})
-		.attr('r', function(){
+		.attr('r', function() {
 			return 2;
 		})
-		.attr('class', function(d){
+		.attr('class', function(d) {
 			return 'appearance ' + d.character.affiliation;
 		});
 
@@ -221,41 +246,36 @@ console.log(obj);
 
 
 
-
-
-
-
-
-
-
+	// console.log('**********')
+	// console.dir(obj[10].appearances)
+	// console.dir(obj[10].images)
+	// console.log('**********')
 
 	// Draw figures at time periods
-	svg.selectAll('.scene').selectAll('.appearance2').data(function(d) {
-		return d.appearances;
-	}).enter().call(function(s) {
-		var g, text;
+	// Since figures are added after the fact, split these into two different sections.
+	// The 2nd time around selecting add the correct figure names. It would be nice
+	// in future to structure data by merging both into one.
 
-		g = s.append('g').attr('class', 'label');
+	// First create the images
+	svg.selectAll('.scene').selectAll('.appearanceimg').data(
+		function(obj) { return obj.appearances; }
+	).enter().call(function(s) {
+		var d, g, text;
 
+		// d = obj.appearances;
+
+		g = s.append('g').attr('class', function(d) {
+			return 'appearanceimg image ' + d.character.affiliation;
+		});
+//===================================================================
 		g.append('svg:image')
-			.attr('y', -30)
-			.attr('x', -sceneWidth*0.5 )
-			.attr('width', sceneWidth)
+			.attr('y', -20)
+			.attr('x', -sceneWidth * 0.5)
+			.attr('width', sceneWidth*2)
 			.attr('height', 400 * 0.15)
-			.attr('transform', 'rotate(90 0 0)')
-			// .attr('xlink:href', './images/Image1.jpg');
-			// .attr('xlink:href', './images/14.25.32_002dec2014.jpg');
-			.attr('xlink:href', 'fff');
-			// .text( function(d){ return d.images  } );
-
-
-
-		text = g.append('g').attr('class', 'text');
-
-		// Apppend two actual 'text' nodes to fake an 'outside' outline.
-		text.append('text');
-		text.append('text').attr('class', 'color')
-		// .text(function(d){ return 'fpath' + d.images.imagenames[0]  })
+			// .attr('transform', 'rotate(90 0 0)')
+			.attr('xlink:href', function(d) { return './prepareDataset/downloads/' + d.imagename }); 
+//===================================================================
 
 		g.attr('transform', function(d) {
 			var x, y;
@@ -264,18 +284,32 @@ console.log(obj);
 			return 'translate(' + [x, y] + ')';
 		});
 
-
-
+		// text = g.append('g').attr('class', 'text');
+		// g.append('text').attr('class', 'color')
+		// .text('as'
+		// function(d) {return d.character.id;}
+		// );
 	});
 
+	// // use for debugging filenames
+	// svg.selectAll('.scene').selectAll('.appearanceimg').data(
+	// 		function(obj) { return obj.appearances; }
+	// 	).append('text').attr('class', 'addedcolor')
+	// 	.text(function(d) {
+	// 		return d.imagename
+	// 	});
 
 
 
 
 
 
-// ----------------------------------------------
 
+
+	// ---------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------
+
+	// console.log(narrative.introductions())
 	// Draw intro nodes
 	svg.selectAll('.intro').data(narrative.introductions())
 		.enter().call(function(s) {
@@ -283,19 +317,11 @@ console.log(obj);
 
 			g = s.append('g').attr('class', 'intro');
 
-			// g.append('rect')
-			// 	.attr('y', -4)
-			// 	.attr('x', -4)
-			// 	.attr('width', 4)
-			// 	.attr('height', 8);
-
-			// g.append('svg:image')
-			// 	.attr('y', -30)
-			// 	.attr('x', -10)
-			// 	.attr('width', 225 * 0.15)
-			// 	.attr('height', 400 * 0.15)
-			// 	.attr('xlink:href', './images/Image1.jpg');
-
+			g.append('rect')
+				.attr('y', -4)
+				.attr('x', -4)
+				.attr('width', 4)
+				.attr('height', 8);
 
 			text = g.append('g').attr('class', 'text');
 
@@ -305,7 +331,7 @@ console.log(obj);
 
 			g.attr('transform', function(d) {
 				var x, y;
-				x = Math.round(d.x);
+				x = Math.round(d.x) ;
 				y = Math.round(d.y);
 				return 'translate(' + [x, y] + ')';
 			});
@@ -313,7 +339,7 @@ console.log(obj);
 			g.selectAll('text')
 				.attr('text-anchor', 'end')
 				.attr('y', '5px')
-				.attr('x', '-20px')
+				.attr('x', '-10px')
 				.text(function(d) { return d.character.name; });
 
 			g.select('.color')
@@ -329,6 +355,14 @@ console.log(obj);
 		});
 
 });
+
+
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+
+
 
 function wrangle(data) {
 
